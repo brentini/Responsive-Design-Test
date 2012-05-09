c7d32095f5c1538d234ba4282809d3d189559e1c
brentini <brentroos@gmail.com>
2012-05-09 16:32:06 -0500

    Update responsive.js

--- responsive.js
+++ responsive.js
@@ -1,4 +1,4 @@
-var defaultURL = 'peoriawebsolutions.com'; //<---- CHANGE TO YOUR WEBSITE URL
+var defaultURL = 'getskeleton.com'; //<---- CHANGE TO YOUR WEBSITE URL
 
 //show loading graphic
 function showLoader(id) {
